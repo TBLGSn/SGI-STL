18e89b72717fdaa4940c4c3d626eb13bb061a21b
1844760779@qq.com <1844760779@qq.com>
2021-04-25 12:01:16 +0800

    删除 Annotation文件夹中的版权声明章节

--- Annotation/iterator/type_traits.h
+++ Annotation/iterator/type_traits.h
@@ -1,17 +1,3 @@
-/*
- *
- * Copyright (c) 1997
- * Silicon Graphics Computer Systems, Inc.
- *
- * Permission to use, copy, modify, distribute and sell this software
- * and its documentation for any purpose is hereby granted without fee,
- * provided that the above copyright notice appear in all copies and
- * that both that copyright notice and this permission notice appear
- * in supporting documentation.  Silicon Graphics makes no
- * representations about the suitability of this software for any
- * purpose.  It is provided "as is" without express or implied warranty.
- */
-
 #ifndef __TYPE_TRAITS_H
 #define __TYPE_TRAITS_H
 
